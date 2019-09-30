769661d2c9a8c72b3e4caa1618f311473e96250e
simonmonk <evilgeniusauthor@gmail.com>
2019-09-30 12:13:38 +0100

    improved dates

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(scheduled_for: :asc)
   end
 
   # GET /tweets/1
